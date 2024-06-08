b44e684b4e91ec49e847750dd6b54eb5993f092b
Ayush Abad <yush@pop-os.localdomain>
2024-06-08 22:00:00 +0530

    I'm tweakin,rn

---
+++ src/features/JournalBox.jsx/JournalBox.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../../ui/Navbar";
+const JournalBox = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>background</div>
+    </div>
+  );
+};
+export default JournalBox;
